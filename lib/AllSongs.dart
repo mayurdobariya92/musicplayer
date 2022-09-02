99a9c4822d873b9e4c6b113f281df9b7ae4538b2
admin <ohobye2@gmail.com>
2022-09-02 17:59:15 +0530

    Initial commit

--- lib/AllSongs.dart
+++ lib/AllSongs.dart
@@ -9,20 +9,79 @@ class AllSongs extends StatefulWidget {
   State<AllSongs> createState() => _AllSongsState();
 }
 
-class _AllSongsState extends State<AllSongs> {
+                                    ////// musicpalyer Stop and Palay code /////
 
- AudioPlayer player = AudioPlayer();
+class _AllSongsState extends State<AllSongs> {
+  AudioPlayer player = AudioPlayer();
+  List<bool> statuslist = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    statuslist = List.filled(Model.songlist.length, false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("All Songs"),),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(onTap: () async {
-          await player.play(DeviceFileSource(Model.songlist[index].data));
+      appBar: AppBar(
+        title: Text("All Songs"),
+      ),
+      body: ListView.builder(
+          itemCount: Model.songlist.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              trailing: statuslist[index]
+                  ? IconButton(
+                      onPressed: () async {
+                        setState(() {
+                          statuslist =
+                              List.filled(Model.songlist.length, false);
+                        });
+                        print(statuslist);
 
-        },title: Text("${Model.songlist[index].displayNameWOExt}"),
-    subtitle: Text("${Model.songlist[index].duration}"),);
-      }),
+                        await player.stop();
+                      },
+                      icon: Icon(Icons.pause))
+                  : IconButton(
+                      onPressed: () async {
+                        await player.stop();
+                        setState(() {
+                          statuslist =
+                              List.filled(Model.songlist.length, false);
+                          statuslist[index] = true;
+                        });
+                        await player
+                            .play(DeviceFileSource(Model.songlist[index].data));
+
+                        print(statuslist);
+                      },
+                      icon: Icon(Icons.play_arrow)),
+              title: Text("${Model.songlist[index].displayNameWOExt}"),
+              subtitle: Text("${Model.songlist[index].duration}"),
+            );
+          }),
     );
   }
 }
+
+///// only music player Song list code/////
+
+// class _AllSongsState extends State<AllSongs> {
+//
+//   AudioPlayer player = AudioPlayer();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("All Songs"),),
+//       body: ListView.builder(itemBuilder: (context, index) {
+//         return ListTile(onTap: () async {
+//           await player.play(DeviceFileSource(Model.songlist[index].data));
+//
+//         },title: Text("${Model.songlist[index].displayNameWOExt}"),
+//           subtitle: Text("${Model.songlist[index].duration}"),);
+//       }),
+//     );
+//   }
+// }
